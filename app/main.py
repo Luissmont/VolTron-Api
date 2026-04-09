32a9fb55eb418fe2082bba71d0ba0a27e75fd6b7
Luissmont <luispm2424@gmail.com>
2026-04-08 20:25:39 -0600

    Cors

--- app/main.py
+++ app/main.py
@@ -11,6 +11,14 @@ app = FastAPI(
     version="1.0.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 async def root():
     return {"message": "La api anda al tiro"}
