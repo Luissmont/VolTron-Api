539766cd230a0c28237b1300408b85974fba4608
Luissmont <luispm2424@gmail.com>
2026-04-08 19:59:12 -0600

    configs y data del user

---
+++ app/db/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from app.core.config import settings
+
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=True, 
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine, 
+    expire_on_commit=False
+)
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
