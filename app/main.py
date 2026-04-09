c7be696947dea95689442e8348fe4861dfd5644b
Luissmont <luispm2424@gmail.com>
2026-04-08 20:21:40 -0600

    Conexión, rutas base y health de conexión

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db.database import get_db
+
+app = FastAPI(
+    title="VolTron API",
+    description="Api de Componentes",
+    version="1.0.0"
+)
+
+@app.get("/")
+async def root():
+    return {"message": "La api anda al tiro"}
+
+@app.get("/health")
+async def health_check(db: AsyncSession = Depends(get_db)):
+    try:
+        await db.execute(text("SELECT 1"))
+        return {"status": "ok", "database": "Conectada bien"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error conectando a la BD: {str(e)}")
